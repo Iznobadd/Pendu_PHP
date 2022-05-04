b2f0012dddc4104a952930464c0154a9ea591dcd
Brandon CEBI <brandon.cebi.pro@gmail.com>
2022-05-04 11:07:57 +0200

    ajout des vies restantes

--- index.php
+++ index.php
@@ -6,26 +6,25 @@ $toFind = $dico[array_rand($dico)];
 
 $proposition = trim(readline("Merci de saisir un mot pour tenter de trouver le mot mystère : "));
 
+$life = 5;
+
 // While simple 
 
 while ($proposition !== $toFind) {
-    echo $proposition . " nest pas le mot à trouver , veuillez tenter de nouveau " . PHP_EOL;
-    $proposition = trim(readline("nouvelle tentative : "));
+    $life--;
+
+    if($life == 0 )
+    {
+        echo "Vous avez épuisé toutes vos vies, vous avez perdu!";
+        exit;
+    }
+    else
+    {
+        echo $proposition . " nest pas le mot à trouver, il vous reste " . $life . "vies, veuillez tenter de nouveau " . PHP_EOL;
+
+        $proposition = trim(readline("nouvelle tentative : "));
+    }
+
 }
 
 echo "Bravo" . PHP_EOL;
-
-// do...while avec un système de flag ==> $jeuEnCours
-
-$jeuEnCours = true;
-
-do {
-    $proposition = trim(readline("Merci de saisir un mot pour tenter de trouver le mot mystère : "));
-    if ($proposition === $toFind) {
-        echo "Bravo !";
-        $jeuEnCours = false;
-    } else {
-        echo $proposition .  'n\' est pas le mot à trouver vous pouvez tenter de nouveau ';
-    }
-    echo PHP_EOL;
-} while ($jeuEnCours);
