602b3c18a10092d01b37471fae22b882d39b5605
Brandon CEBI <brandon.cebi.pro@gmail.com>
2022-05-05 14:03:17 +0200

    Ajout d'une lettre en proposition

--- index.php
+++ index.php
@@ -8,11 +8,16 @@ $proposition = trim(readline("Merci de saisir un mot pour tenter de trouver le m
 
 $life = 5;
 
-// While simple 
+$etat = "";
+
+for($i = 0; $i < strlen($toFind); $i++)
+{
+    var_dump($toFind[$i]);
+    $etat .= '_';
+}
 
 while ($proposition !== $toFind) {
     $life--;
-
     if($life == 0 )
     {
         echo "Vous avez épuisé toutes vos vies, vous avez perdu!";
@@ -20,11 +25,40 @@ while ($proposition !== $toFind) {
     }
     else
     {
-        echo $proposition . " nest pas le mot à trouver, il vous reste " . $life . "vies, veuillez tenter de nouveau " . PHP_EOL;
+        // SAISIE D'UNE LETTRE
+        if(strlen($proposition) == 1)
+        {
 
-        $proposition = trim(readline("nouvelle tentative : "));
-    }
+            for ($i = 0; $i < strlen($toFind); $i++) {
+                if ($toFind[$i] === $proposition) {
+                    $etat[$i] = $proposition;
+
+                    if($etat == $toFind)
+                    {
+                        echo "Bravo vous avez trouvé le mot !";
+                        exit;
+                    }
+                }
+            }
+
+            echo $etat . PHP_EOL;
+            echo ' La lettre ' . $proposition . " n'est pas dans le mot. Il vous reste " . $life . "vies, veuillez tenter de nouveau " . PHP_EOL;
+
+            $proposition = trim(readline("nouvelle tentative : "));
+
+        }
+
+        else
+            {
+                echo ' ' . $proposition . " nest pas le mot à trouver, il vous reste " . $life . "vies, veuillez tenter de nouveau " . PHP_EOL;
+
+                $proposition = trim(readline("nouvelle tentative : "));
+            }
+        }
 
 }
 
 echo "Bravo" . PHP_EOL;
+
+
+?>
